f9c57178d5ad8e90facb523a7cfc833cff2c7aa2
Johnny-Williams729 <johnnyw2179@gmail.com>
2026-02-18 10:51:18 +0000

    Base class created

--- inc/dataImporter.hpp
+++ inc/dataImporter.hpp
@@ -1,4 +1,19 @@
+#include "grade.hpp"
+#include <fstream>
+#include <string>
+#include "json.hpp"
+#include <array>
+using json = nlohmann::json;
+
+
 
 struct dataImporter
 {
+    dataImporter(const std::string& _filename);
+    void load(std::array<grade, 10000>& arr);
+
+
+    std::string filename;
+    std::ifstream file;
+
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,22 @@
 int main(int argc, char* argv[])
 {
 
-	grade G{}; // this is just done so that the compiler
+	std::array<grade,10000> Grades;
+
+	//grade G{}; // this is just done so that the compiler
 		   // will read it and check for errors.  it is
 		   // not needed for anything else.
 
 	// create a dataImporter
-	std::string filename = "some valid filename.json";
-	dataImporter DI{};  // DI is Data Importer....
+	std::string filename = "allgradedata.json";
+	dataImporter DI(filename);  // DI is Data Importer....
 			    
 	// actually import the data into the array
 	//grade AOG[100];                  // this needs to be in the grade header...
 	//unsigned int maxSizeOfArray=100; // as does this
         //unsigned int num = DI.readFromFile(filename,arrayOfgrades,maxSizeofArray);	
+
+	DI.load(Grades);
 
 	// perhaps here we need to do some processing.  or maybe it is done
 	// after each command???
@@ -26,8 +30,12 @@ int main(int argc, char* argv[])
         // create the commandProcessor
 	std::cout << "Welcome to the command line MavGrades" << std::endl;
 	// while input commands are OK, process the commands
-	// {
-	// }
+	{
+		std::string cmd;
+		std::cin >> cmd;
+		Grades[stoi(cmd)].print();
+		std::cout << "\nNumber of grade datas: " << grade::NumOfGradeDatas << std::endl;
+	}
 
 	// clean up
 	return 0;
