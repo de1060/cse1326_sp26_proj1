8986876196430c390cc344fae4af293da9b8e7f1
de1060 <dxe1060@mavs.uta.edu>
2026-02-18 15:47:07 -0600

    Update main.cpp
    Clean slate.

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,6 @@
 int main(int argc, char* argv[])
 {
 
-	std::array<grade,10000> Grades;
 
-	//grade G{}; // this is just done so that the compiler
-		   // will read it and check for errors.  it is
-		   // not needed for anything else.
-
-	// create a dataImporter
-	std::string filename = "allgradedata.json";
-	dataImporter DI(filename);  // DI is Data Importer....
-			    
-	// actually import the data into the array
-	//grade AOG[100];                  // this needs to be in the grade header...
-	//unsigned int maxSizeOfArray=100; // as does this
-        //unsigned int num = DI.readFromFile(filename,arrayOfgrades,maxSizeofArray);	
-
-	DI.load(Grades);
-
-	// perhaps here we need to do some processing.  or maybe it is done
-	// after each command???
-
-
-        // create the commandProcessor
-	std::cout << "Welcome to the command line MavGrades" << std::endl;
-	// while input commands are OK, process the commands
-	{
-		std::string cmd;
-		std::cin >> cmd;
-		Grades[stoi(cmd)].print();
-		std::cout << "\nNumber of grade datas: " << grade::NumOfGradeDatas << std::endl;
-	}
-
-	// clean up
 	return 0;
 }
