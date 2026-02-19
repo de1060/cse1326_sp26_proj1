91ec21bc341ab8cdad6d4751f7640f48d3cabe1c
de1060 <dxe1060@mavs.uta.edu>
2026-02-19 17:44:36 -0600

    Update grade.hpp

--- inc/grade.hpp
+++ inc/grade.hpp
@@ -1,38 +1,5 @@
 // This class holds the grade date for one course.
-#pragma once
-
-#include <string>
-#include <iostream>
-
-
-struct GradeCounts{
-	int A,B,C,D,F,I,P,Q,W,Z,R;
-};
-
-
-	struct grade
+struct grade
 {
-	static int NumOfGradeDatas;
 	grade();
-	void print();
-	
-	std::string subject_id;
-	int course_number;
-    std::string course_title;
-	std::string section_number;
-	GradeCounts grades;
-	int grades_count;
-	int year;
-	std::string semester;
-	std::string career;
-	std::string instructor1;
-	std::string instructor2;
-	std::string instructor3;
-	std::string instructor4;
-	std::string instructor5;
-	double course_gpa;
-	double drop_percent;
-
-
-
 };
