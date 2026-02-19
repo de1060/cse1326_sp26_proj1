e67afb1cde5ab8da941135b7071ff4b3eca2514c
de1060 <dxe1060@mavs.uta.edu>
2026-02-19 17:43:33 -0600

    Update grade.cpp

--- src/grade.cpp
+++ src/grade.cpp
@@ -1,44 +1,6 @@
 #include "grade.hpp"
 
-int grade::NumOfGradeDatas = 0;
-
 grade::grade()
-{	
-	NumOfGradeDatas++;
-	// this is a highly sophisticated constructor
-	
-
-
-	
-}
-void grade::print()
 {
-	std::cout 	<< "Subject ID: " << subject_id
-				<< "\nCourse number: " << course_number
-				<< "\nCourse title: " << course_title
-				<< "\nSection number: " << section_number
-				<< "\nGrades:"
-				<< "\nA: " << grades.A
-				<< "\nB: " << grades.B
-				<< "\nC: " << grades.C
-				<< "\nD: " << grades.D
-				<< "\nF: " << grades.F
-				<< "\nI: " << grades.I
-				<< "\nP: " << grades.P
-				<< "\nQ: " << grades.Q
-				<< "\nW: " << grades.W
-				<< "\nZ: " << grades.Z
-				<< "\nR: " << grades.R
-				<< "\nGrades count: " << grades_count
-				<< "\nYear: " << year
-				<< "\nSemester: " << semester
-				<< "\nCareer: " << career
-				<< "\nInstructor 1: " << instructor1
-				<< "\nInstructor 2: " << instructor2
-				<< "\nInstructor 3: " << instructor3
-				<< "\nInstructor 4: " << instructor4
-				<< "\nInstructor 5: " << instructor5
-				<< "\nCourse GPA: " << course_gpa
-				<< "\nDrop Percent: " << drop_percent << std::endl;
-	return;
+	// this is a highly sophisticated constructor
 }
