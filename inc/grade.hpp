6b54fb27324b48b8fee206990a70dcbf21521b97
de1060 <dxe1060@mavs.uta.edu>
2026-02-19 18:01:21 -0600

    Update grade.hpp

--- inc/grade.hpp
+++ inc/grade.hpp
@@ -3,3 +3,58 @@ struct grade
 {
 	grade();
 };
+
+
+//Sample to see how the data looks. 
+
+/*
+{
+      "subject_id": "SPAN",
+      "course_number": "5303",
+      "course_title": "APPLIED SPANISH LINGUISTICS",
+      "section_number": "001",
+      "grades": {
+        "A": "5",
+        "B": "2",
+        "C": "0",
+        "D": "0",
+        "F": "0",
+        "I": "0",
+        "P": "2",
+        "Q": "0",
+        "W": "1",
+        "Z": "0",
+        "R": "0"
+      },
+      "grades_count": "10",
+      "year": "2020",
+      "semester": "Fall",
+      "career": "GRAD",
+      "instructor1": "A Elliott",
+      "instructor2": "",
+      "instructor3": "",
+      "instructor4": "",
+      "instructor5": "",
+      "course_gpa": 3.78,
+      "drop_percent": 8.33
+    },
+    {
+      "subject_id": "ECON",
+      "course_number": "5337",
+      "course_title": "BUSINESS & ECONOMIC FORECASTING",
+      "section_number": "001",
+      "grades": {
+        "A": "9",
+        "B": "7",
+        "C": "0",
+        "D": "3",
+        "F": "1",
+        "I": "0",
+        "P": "4",
+        "Q": "0",
+        "W": "2",
+        "Z": "0",
+        "R": "0"
+      },
+
+*/
