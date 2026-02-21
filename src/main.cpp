866a72b560d2bada30e49c7d36a3930ae4ee5467
de1060 <dxe1060@mavs.uta.edu>
2026-02-20 18:50:04 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
 #include "grade.hpp"
+#include <iostream>
+#include <string>
+
 
 int main(int argc, char* argv[])
 {
+	std::array<grade, 10000> GradeArray;
+
+
+	
 	grade G{}; // this is just done so that the compiler
 		   // will read it and check for errors.  it is
 		   // not needed for anything else.
